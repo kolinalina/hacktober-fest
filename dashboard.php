e469468d8bde980dcba7cf8d23e621463c88ec95
unknown <ganufvyandana@gmail.com>
2019-10-18 09:17:28 +0700

    add text

--- dashboard.php
+++ dashboard.php
@@ -53,6 +53,7 @@
                     <div class="row mb-2">
                         <div class="col-sm-6">
                             <h1 class="m-0 text-dark"><small>Hallo</small><b> <?=$_SESSION['nama']?></b></h1>
+                            <h2> Ini Adalah Halaman Dashboard </h2>
                         </div>
                     </div>
                 </div>
