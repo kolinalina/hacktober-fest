88d651085e0050baf543eb45172a3fd7262588b4
unknown <ganufvyandana@gmail.com>
2019-10-18 13:05:53 +0700

    welcome

--- dashboard.php
+++ dashboard.php
@@ -55,6 +55,7 @@
                             <h1 class="m-0 text-dark"><small>Hallo</small><b> <?=$_SESSION['nama']?></b></h1>
                             <h2> Ini Adalah Halaman Dashboard </h2>
                             <hr>
+                            <p>Selamat Datang</p>
                         </div>
                     </div>
                 </div>
